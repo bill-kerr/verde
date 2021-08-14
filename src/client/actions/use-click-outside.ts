b6444152ad2e0dc703fd0614199376548f371c71
Bill Kerr <bill@tailwindapp.com>
2021-08-14 14:11:13 -0500

    Add click outside

--- src/client/actions/use-click-outside.ts
+++ src/client/actions/use-click-outside.ts
@@ -1,11 +1,55 @@
-const clickOutside = (node: HTMLElement) => {
-  const state: ClickOutsideState = {
-    isPointerDown: false;
-  }
+type ClickOutsideState = {
+	isPointerDown: boolean;
+};
 
-  const onPointerDown = () => {
+type ClickOutsideParams = {
+	onClickOutside: (e: Event) => void;
+	rootElement?: HTMLElement;
+};
 
-  };
+const isValidEvent = (event: Event | MouseEvent, node: HTMLElement, rootElement: HTMLElement | Document = document) => {
+	if (('button' in event && event.button > 0) || event.defaultPrevented) return false;
 
-  document.addEventListener('mousedown', onPointerDown, true)
+	const target = event.target as HTMLElement | null;
+
+	// If the target is no longer in the root element
+	if (target && !rootElement.contains(target)) return false;
+
+	return !node.contains(target);
+};
+
+export const clickOutside = (node: HTMLElement, params: ClickOutsideParams) => {
+	const state: ClickOutsideState = {
+		isPointerDown: false,
+	};
+
+	const onPointerDown = (e: Event) => {
+		if (isValidEvent(e, node)) {
+			state.isPointerDown = true;
+		}
+	};
+
+	const onPointerUp = (e: Event) => {
+		if (state.isPointerDown && isValidEvent(e, node)) {
+			state.isPointerDown = false;
+			params.onClickOutside(e);
+		}
+	};
+
+	document.addEventListener('mousedown', onPointerDown, true);
+	document.addEventListener('mouseup', onPointerUp, true);
+	document.addEventListener('touchstart', onPointerDown, true);
+	document.addEventListener('touchend', onPointerUp, true);
+
+	return {
+		update(newParams: ClickOutsideParams) {
+			params = newParams;
+		},
+		destroy() {
+			document.removeEventListener('mousedown', onPointerDown, true);
+			document.removeEventListener('mouseup', onPointerUp, true);
+			document.removeEventListener('touchstart', onPointerDown, true);
+			document.removeEventListener('touchend', onPointerUp, true);
+		},
+	};
 };
--- src/client/pages/dashboard.svelte
+++ src/client/pages/dashboard.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+	import { clickOutside } from '$lib/client/actions/use-click-outside';
+
+	let toggled = false;
+
+	const handleClickOutside = () => {
+		console.log('clicked outside');
+	};
+
+	const otherHandleClickOutside = () => {
+		console.log('this is the new clicked outside');
+	};
+
+	$: handler = toggled ? otherHandleClickOutside : handleClickOutside;
 </script>
 
 <div class="min-h-screen bg-gradient-to-t from-lime-50 to-emerald-100 bg-fixed">
@@ -8,6 +21,8 @@
 				<button type="button" class="inline-block px-4 py-2 rounded-lg">+ Add an expense</button>
 				<button type="button" class="inline-block px-4 py-2 rounded-lg">Do nothing</button>
 			</nav>
+			<div use:clickOutside={{ onClickOutside: handler }} class="bg-blue-300 h-40 w-40 rounded">inside</div>
+			<button on:click={() => (toggled = !toggled)}>toggle</button>
 		</div>
 	</div>
 </div>
